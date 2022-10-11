99c5404fced3ecdd634b215d44e6714fd8b32e56
santhosh kumar yaarabati <santhoshkumaryaarabati@santhoshs-MacBook-Air.local>
2022-10-11 17:45:23 +0530

    test commit 1

--- src/components/CommitsList.js
+++ src/components/CommitsList.js
@@ -3,12 +3,13 @@ import moment from 'moment';
 import '../App.css';
 
 const CommitsList = ({ commits }) => {
+    console.log()
     if (commits.length === 0) {
         return <div>No commits to display</div>
     }
     return (
         <>
-        {commits.map(commit => (<div key={commit.author.date} className="commit-container">
+        {commits.map((commit, ind) => (<div key={ind} className="commit-container">
             <h3>{commit.commit.message}</h3>
             <p>{moment(commit.commit.author.date).format("MMM Do YY, h:mm a")} by {commit.commit.author.name}</p>
         </div>))}
